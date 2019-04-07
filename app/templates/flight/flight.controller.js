3e15eb652c7f9302f7aa60c5e2e19dc8b20b793b
Valentine <kravcovvalentin@gmail.com>
2019-04-07 23:14:03 +0300

    update flight page

--- app/templates/flight/flight.controller.js
+++ app/templates/flight/flight.controller.js
@@ -8,5 +8,25 @@
 
     function FlightController(flightService, flights) {
         console.log(flights);
+        $(document).ready(function () {
+
+            // accordion effect
+
+           let acc = document.getElementsByClassName("accordion");
+            let i;
+
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function () {
+                    this.classList.toggle("active");
+                    var panel = this.nextElementSibling;
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                });
+            }
+
+        });
     }
 })();
