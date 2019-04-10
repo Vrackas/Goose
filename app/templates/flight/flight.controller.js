405d5a250824d2167dc58c274221db6397ebf3fa
Vitalii Stalynskyi <vista-54@ukr.net>
2019-04-11 00:18:08 +0300

    Added filters. Included API

--- app/templates/flight/flight.controller.js
+++ app/templates/flight/flight.controller.js
@@ -10,26 +10,59 @@
         // console.log(flights);
         var vm = this;
         vm.flights = flights.flights;
+        vm.filterByStops = filterByStops;
+        vm.transformDate = transformDate;
+        vm.filterByDuration = filterByDuration;
 
+        function transformDate(date) {
+            return date.substr(11, 5)
+        }
+
+        function filterByStops(count, checked, more) {
+            if (checked) {
+                vm.flights = flights.flights;
+            }
+            else {
+                vm.flights = vm.flights.filter(function (item) {
+                    if (more) {
+                        return item.legs.length >= count;
+                    }
+                    return item.legs.length === count;
+                })
+            }
+        }
+
+        function filterByDuration(hour_s, hour_f, checked) {
+            if (checked) {
+                vm.flights = flights.flights;
+            } else {
+                vm.flights = vm.flights.filter(function (item) {
+                    return (item.flightTimeHour >= hour_s && item.flightTimeHour <= hour_f);
+                })
+            }
+
+        }
 
         $(document).ready(function () {
 
             // accordion effect
+            setTimeout(function () {
+                let acc = document.getElementsByClassName("accordion");
+                let i;
 
-            let acc = document.getElementsByClassName("accordion");
-            let i;
+                for (i = 0; i < acc.length; i++) {
+                    acc[i].addEventListener("click", function () {
+                        this.classList.toggle("active");
+                        var panel = this.nextElementSibling;
+                        if (panel.style.maxHeight) {
+                            panel.style.maxHeight = null;
+                        } else {
+                            panel.style.maxHeight = panel.scrollHeight + "px";
+                        }
+                    });
+                }
+            }, 3000)
 
-            for (i = 0; i < acc.length; i++) {
-                acc[i].addEventListener("click", function () {
-                    this.classList.toggle("active");
-                    var panel = this.nextElementSibling;
-                    if (panel.style.maxHeight) {
-                        panel.style.maxHeight = null;
-                    } else {
-                        panel.style.maxHeight = panel.scrollHeight + "px";
-                    }
-                });
-            }
 
         });
     }
